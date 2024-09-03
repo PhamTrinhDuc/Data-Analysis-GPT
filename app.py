a048092f914532399df26be9d156c817d0a8abc5
PhamTrinhDuc <duc78240@gmail.com>
2024-09-03 14:57:29 +0700

    update

--- app.py
+++ app.py
@@ -1,37 +1,39 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 from langchain_experimental.agents.agent_toolkits.pandas.base import create_pandas_dataframe_agent
-from langchain.agents.agent_types import AgentType
 from source.model import load_openai_model
 from source.utils import excutor_code
-from logs.logger import set_logging_terminal, set_logging_error
-from source.tool_visualize import render
+from logs.logger import set_logging_terminal
 
-logger_error = set_logging_error()
+
 logger_terminal = set_logging_terminal()
 
+model = load_openai_model()
 
-def process_query(agent: create_pandas_dataframe_agent, query: str):
-    response  = agent(query)
 
-    action = response['intermediate_steps'][-1][0].tool_input["query"]
+def process_query(da_agent, query):
+
+    response = da_agent(query)
+
+    action = response["intermediate_steps"][-1][0].tool_input["query"]
 
     if "plt" in action:
-        st.write(response['output'])
-        fig = excutor_code(action, df = st.session_state.df)
+        st.write(response["output"])
+
+        fig = excutor_code(action, df=st.session_state.df)
         if fig:
             st.pyplot(fig)
-        
+
         st.write("**Executed code:**")
         st.code(action)
 
-        to_display_string = response['output'] + "\n" + f"```python\{action}\n```"
+        to_display_string = response["output"] + "\n" + f"```python\n{action}\n```"
         st.session_state.history.append((query, to_display_string))
-    
+
     else:
-        st.write(response['output'])
-        st.seasion_state.history((query, response['output']))
+        st.write(response["output"])
+        st.session_state.history.append((query, response["output"]))
+
 
 def display_chat_history():
     st.markdown("## Chat History: ")
@@ -40,47 +42,55 @@ def display_chat_history():
         st.markdown(f"**Response: {i+1}:** {r}")
         st.markdown("---")
 
+
 def main():
 
-    render()
-    # st.set_page_config(page_title="📊 Smart Data Analysis Tool", page_icon="📊", layout="centered")
-    # st.header("📊 Smart Data Analysis Tool")
-    # st.write(
-    #     "### Welcome to our data analysis tool. This tools can assist your daily data analysis tasks. Please enjoy !"
-    # )
+    # Set up streamlit interface
+    st.set_page_config(page_title="📊 Smart Data Analysis Tool", page_icon="📊", layout="centered")
+    st.header("📊 Smart Data Analysis Tool")
+    st.write(
+        "### Welcome to our data analysis tool. This tools can assist your daily data analysis tasks. Please enjoy !"
+    )
 
+    # Load llms model
     llm = load_openai_model()
-    logger_terminal.info("Model loaded successfully")
+    logger_terminal.info(f"### Successfully loaded model !###")
 
+    # Upload csv file
     with st.sidebar:
-        uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    
+        uploaded_file = st.file_uploader("Upload your csv file here", type="csv")
+
     # Initial chat history
     if "history" not in st.session_state:
         st.session_state.history = []
 
+    # Read csv file
     if uploaded_file is not None:
         st.session_state.df = pd.read_csv(uploaded_file)
-        st.write(st.session_state.df)
-        st.write("### Data loaded successfully", st.session_state.df.head())
+        st.write("### Your uploaded data: ", st.session_state.df.head())
 
-        agent = create_pandas_dataframe_agent(
+        # Create data analysis agent to query with our data
+        da_agent = create_pandas_dataframe_agent(
             llm=llm,
             df=st.session_state.df,
-            agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            agent_type="tool-calling",
             allow_dangerous_code=True,
-            verbose=True,
+            verbose=False,
             return_intermediate_steps=True,
         )
+        logger_terminal.info("### Sucessfully loaded data analysis agent !###")
 
-        logger_terminal.info("Agent created successfully")
+        # Input query and process query
+        query = st.text_input("Enter your questions: ")
 
         if st.button("Run query"):
-                query = st.text_area("Enter your query here")
-                process_query(agent, query)
+            with st.spinner("Processing..."):
+                process_query(da_agent, query)
 
-        if st.button("Show history"):
-            display_chat_history()
+    # Display chat history
+    st.divider()
+    display_chat_history()
+
 
 if __name__ == "__main__":
     main()
--- source/tool_visualize.py
+++ source/tool_visualize.py
@@ -2,20 +2,24 @@ import streamlit as st
 from pygwalker.api.streamlit import StreamlitRenderer
 
 
-def render():
+def main():
+
+    # Set up Streamlit interface
     st.set_page_config(
-        page_title="📈 Interactive Visualization Tool",
-        page_icon="📈",
-        layout="wide",
+        page_title="📈 Interactive Visualization Tool", page_icon="📈", layout="wide"
     )
 
     st.header("📈 Interactive Visualization Tool")
-    st.write(" Welcome to interactive visualization tool. Please enjoy !")
+    st.write("### Welcome to interactive visualization tool. Please enjoy !")
 
+    # Render pygwalker
     if st.session_state.get("df") is not None:
-        pyg_app = StreamlitRenderer(
-            st.session_state.df
-        )
+        pyg_app = StreamlitRenderer(st.session_state.df)
         pyg_app.explorer()
+
     else:
         st.info("Please upload a dataset to begin using the interactive visualization tools")
+
+
+if __name__ == "__main__":
+    main()
